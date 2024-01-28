768ed2fc771ca969ebcdef9c0ac9f31fdf090205
Andus <30293380+AndusDEV@users.noreply.github.com>
2024-01-28 17:49:36 +0100

    Update Permissions

--- src/main/java/dev/lavenderpowered/lavender/commands/Permissions.java
+++ src/main/java/dev/lavenderpowered/lavender/commands/Permissions.java
@@ -3,10 +3,8 @@ package dev.lavenderpowered.lavender.commands;
 import net.minestom.server.permission.Permission;
 
 public class Permissions {
-    // Microstom
-    public static final Permission SHUTDOWN = new Permission("bastom.shutdown");
-    public static final Permission RESTART = new Permission("bastom.restart");
-    // Bastom
-    public static final Permission EXTENSIONS = new Permission("bastom.exts");
-    public static final Permission SERVER_INFO = new Permission("bastom.svinfo");
+    public static final Permission SHUTDOWN = new Permission("lavender.shutdown");
+    public static final Permission RESTART = new Permission("lavender.restart");
+    public static final Permission EXTENSIONS = new Permission("lavender.exts");
+    public static final Permission SERVER_INFO = new Permission("lavender.svinfo");
 }
